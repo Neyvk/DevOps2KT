6111597cd76ad60804e16873b6f7c09812a65fa8
Neyas <neyaskin.kv@gmail.com>
2025-10-23 19:02:21 +0300

    hotfix

--- Program.cs
+++ Program.cs
@@ -9,13 +9,18 @@ namespace TimerApp
         static void Main()
         {
             Console.WriteLine("Timer started.");
-            Console.WriteLine("Press 'q' to quit.");
+            Console.WriteLine("Press ESC to stop.");
 
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
             while (true)
             {
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("\nTimer stopped.");
+                    break;
+                }
 
                 var nowTime = stopwatch.Elapsed;
                 Console.Write($"\rtime: {nowTime:hh\\:mm\\:ss}");
